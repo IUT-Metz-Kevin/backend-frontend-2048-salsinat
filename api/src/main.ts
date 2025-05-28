32f58e72e8ebc4505ea29e01ca6cd4cf2512f9b8
Kevin Unfricht <p02865@univ-lorraine.fr>
2025-05-28 08:32:59 +0200

    UPDATE (bootstrap) enable cors

--- api/src/main.ts
+++ api/src/main.ts
@@ -3,6 +3,12 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  // Securité lié à la politique CORS
+  // TriggerWarning: Par défaut acceptera toutes requêtes faites par un nom de domaine différent
+  // Il faudra bien entendu filtrer ça à quelques domaines spécifiques dans le cas où votre API n'est consommé par quelques apps web clientes connues
+  app.enableCors();
+
   await app.listen(3000);
 }
 bootstrap();
